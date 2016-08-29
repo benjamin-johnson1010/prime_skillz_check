0c7935c1581c82967fcbefa8db1c8dbb2c421b2d
BENJAMIN JOHNSON <benjaminjohnson@BENJAMINs-MacBook-Air.local>
2016-08-29 16:28:09 -0500

    new commit

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -2,7 +2,15 @@
 // Each iteration will increase the value
 // of checkz by 1
 var checkz = 0;
-
+for(i=0; i < 5; i++){
+  checkz += 1;
+}
+console.log(checkz);
 // 2 - Write a while loop that runs
+while(i> 2){
+  checkz -= 2;
+  i--;
+}
+console.log(checkz);
 // 3 times. Each iteration will decrease
 // the value of checkz by -2
--- 3-conditionals/if.js
+++ 3-conditionals/if.js
@@ -3,11 +3,17 @@
 // the two variables are equal
 var first = 3;
 var second = 5;
+if(first == second){
+  console.log('Hello');
+}
+
 
 // 2 - Expand your answer above to
 // console log "Goodbye" if
 // the variables are not equal
-
+else{
+  console.log('Goodbye');
+}
 // 3 - Write a statement that
 // assigns the value of the addition
 // of the first & second variables to a new
@@ -15,3 +21,11 @@ var second = 5;
 // than second. If second is greater than first
 // assign the difference of the first & second
 // variables to third.
+if(first > second){
+  var third = first + second;
+
+}
+else if(second > first){
+  var third = second - first;
+}
+console.log(third);
